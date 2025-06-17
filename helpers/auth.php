36724c28df08a8cdcd61378da7424cc28d2e3f07
Ademir Jose Wilsek Scarpim <ademirscarpim@hotmail.com>
2025-06-17 16:43:21 -0300

    add css

--- helpers/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-function verificarAutenticacao() {
-    session_start();
-    if (!isset($_SESSION['usuario'])) {
-        header('Location: ../controllers/AuthController.php?action=login');
-        exit;
-    }
-}
